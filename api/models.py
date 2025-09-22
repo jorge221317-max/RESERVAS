bffb556ce97a758745adb0186271dcf357a04bf9
jorge221317-max <jorge221317@hotmail.com>
2025-09-21 23:52:25 -0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,15 +1,17 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from .database import Base
+from sqlalchemy.orm import relationship
 
 class Usuario(Base):
     __tablename__ = "usuarios"
     id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    rol = Column(String, default="usuario")
+    nombre = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    turnos = relationship("Turno", back_populates="usuario")
 
 class Turno(Base):
     __tablename__ = "turnos"
     id = Column(Integer, primary_key=True, index=True)
-    fecha_hora = Column(DateTime, unique=True, nullable=False)
-    usuario_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
+    fecha_hora = Column(DateTime, unique=True, index=True)
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"))
+    usuario = relationship("Usuario", back_populates="turnos")
