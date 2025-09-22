64c7254c94584d510a7f0116ce176a868b21ad92
jorge221317-max <jorge221317@hotmail.com>
2025-09-22 16:06:05 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from api.routes import router as turnos_router
 import os
 
 app = FastAPI()
 
-# Rutas de archivos estáticos
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Montar archivos estáticos
 app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
 
 # Plantillas
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
+# Incluir rutas de turnos
+app.include_router(turnos_router)
+
 # Ruta principal
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class="html")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
