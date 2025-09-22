a4b2f861d4011f1260f5b528190c760751da8dc8
jorge221317-max <jorge221317@hotmail.com>
2025-09-22 16:48:38 -0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,12 +1,19 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String, DateTime, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+import datetime
 
-metadata = MetaData()
+Base = declarative_base()
 
-turnos = Table(
-    "turnos",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("nombre", String, nullable=False),
-    Column("fecha", String, nullable=False),
-    Column("hora", String, nullable=False),
-)
+class Turno(Base):
+    __tablename__ = "turnos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, nullable=False)
+    fecha_hora = Column(DateTime, default=datetime.datetime.now)
+
+# SQLite simple
+DATABASE_URL = "sqlite:///./turnos.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
