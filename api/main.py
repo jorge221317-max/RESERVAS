de18669e52b4263619207e63885a5e61474e727f
jorge221317-max <jorge221317@hotmail.com>
2025-09-21 22:46:09 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 from .database import Base, engine
 from .routes import router
 
+# Crear tablas si no existen
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Sistema de Reservas")
 app.include_router(router)
 
-# Endpoint raíz para evitar 404
+# Endpoint raíz para verificar que la app está online
 @app.get("/")
 def root():
     return {"message": "Bienvenido al Sistema de Reservas"}
