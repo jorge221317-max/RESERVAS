7fd694db6e5c3d514cb56066e6fa836210d57c00
jorge221317-max <jorge221317@hotmail.com>
2025-09-21 23:20:22 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,22 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from fastapi import Request
-from .database import Base, engine
+from .database import Base, engine, get_db
 from .routes import router
+from sqlalchemy.orm import Session
+from .routes import listar_turnos
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Sistema de Reservas")
 app.include_router(router)
 
-# Templates
 templates = Jinja2Templates(directory="api/templates")
 
-# Endpoint raíz para página interactiva
 @app.get("/")
-def root(request: Request):
-    from .routes import listar_turnos
-    db = next(router.dependant_dependencies[0].dependency())  # obtener session
+def root(request: Request, db: Session = Depends(get_db)):
     turnos = listar_turnos(db)
     return templates.TemplateResponse("turnos.html", {"request": request, "turnos": turnos})
