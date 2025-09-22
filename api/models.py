452ba9720ea859930f4dfaf53c46674c01bbcb8d
jorge221317-max <jorge221317@hotmail.com>
2025-09-22 12:56:14 -0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,18 +1,10 @@
-from databases import Database
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from .database import Base
+from datetime import datetime
 
-DATABASE_URL = "sqlite:///./turnos.db"
-database = Database(DATABASE_URL)
-metadata = MetaData()
+class Turno(Base):
+    __tablename__ = "turnos"
 
-turnos = Table(
-    "turnos",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("nombre", String, nullable=False),
-    Column("fecha", String, nullable=False),
-    Column("hora", String, nullable=False),
-)
-
-engine = create_engine(DATABASE_URL)
-metadata.create_all(engine)
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, index=True)
+    fecha_hora = Column(DateTime, default=datetime.utcnow)
