dff0b23fa6631744f4faa563df8fd50b8e9ea37b
jorge221317-max <jorge221317@hotmail.com>
2025-09-22 10:58:52 -0300

    Update database.py

--- api/database.py
+++ api/database.py
@@ -1,16 +1,8 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, MetaData
+from databases import Database
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./turnos.db"
+DATABASE_URL = "sqlite:///./turnos.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+database = Database(DATABASE_URL)
+metadata = MetaData()
+engine = create_engine(DATABASE_URL)
