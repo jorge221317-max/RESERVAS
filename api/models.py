df155a1262cb1a018925ac69c102836f1f222652
jorge221317-max <jorge221317@hotmail.com>
2025-09-22 02:03:10 -0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,9 +1,8 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from .database import Base
-from datetime import datetime
 
 class Turno(Base):
     __tablename__ = "turnos"
     id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String, nullable=False)
-    fecha_hora = Column(DateTime, default=datetime.now)
+    nombre = Column(String, index=True)
+    fecha_hora = Column(DateTime, index=True)
