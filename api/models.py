5e458562e94a62507cd9cd298d6ebd94c15d1734
jorge221317-max <jorge221317@hotmail.com>
2025-09-22 13:26:28 -0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, Integer, String, DateTime
-from .database import Base
-from datetime import datetime
+from sqlalchemy import Table, Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from .database import metadata
 
-class Turno(Base):
-    __tablename__ = "turnos"
-
-    id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String, index=True)
-    fecha_hora = Column(DateTime, default=datetime.utcnow)
+turnos = Table(
+    "turnos",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("nombre", String, nullable=False),
+    Column("fecha_hora", DateTime, nullable=False),
+)
