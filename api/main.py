d4a1c94443842f70f6e250491c3c8c47edc42ba6
jorge221317-max <jorge221317@hotmail.com>
2025-09-22 12:58:09 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,20 +1,13 @@
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from .routes import router
 
-app = FastAPI(title="API de Reservas 🚀")
-
-# Carpeta static
-app.mount("/static", StaticFiles(directory="api/static"), name="static")
-
-# Carpeta templates
-templates = Jinja2Templates(directory="templates")
-
-# Incluir rutas de API
+app = FastAPI()
 app.include_router(router)
 
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+# Templates
+app.templates = Jinja2Templates(directory="templates")
+
+# Static
+app.mount("/static", StaticFiles(directory="static"), name="static")
