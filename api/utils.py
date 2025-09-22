9d54e9a3d00526896f442d60a6d5757db82645a7
jorge221317-max <jorge221317@hotmail.com>
2025-09-21 22:16:12 -0300

    Update utils.py

--- api/utils.py
+++ api/utils.py
@@ -1,13 +1,24 @@
+import os
 import smtplib
 from email.message import EmailMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASS = os.getenv("EMAIL_PASS")
 
 def enviar_email(destinatario, asunto, cuerpo):
+    if not EMAIL_USER or not EMAIL_PASS:
+        print("EMAIL_USER o EMAIL_PASS no configurados. Email no enviado.")
+        return
+
     msg = EmailMessage()
-    msg['Subject'] = asunto
-    msg['From'] = "jorge221317@gmail.com"       # Cambiá por tu email
-    msg['To'] = destinatario
+    msg["Subject"] = asunto
+    msg["From"] = EMAIL_USER
+    msg["To"] = destinatario
     msg.set_content(cuerpo)
-    
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login("tu_email@gmail.com", "tu_password")
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(EMAIL_USER, EMAIL_PASS)
         smtp.send_message(msg)
