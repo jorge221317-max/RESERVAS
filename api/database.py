4db655d20828be24eb3e6127a7eb14a7f9e33e69
jorge221317-max <jorge221317@hotmail.com>
2025-09-22 02:01:54 -0300

    Update database.py

--- api/database.py
+++ api/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./turnos.db")
+SQLALCHEMY_DATABASE_URL = "sqlite:///./reservas.db"  # Para producción usar PostgreSQL
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
