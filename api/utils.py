ea3f72f788b145d299b995d89494f0e2f1d15e4a
jorge221317-max <jorge221317@hotmail.com>
2025-09-21 23:56:45 -0300

    Update utils.py

--- api/utils.py
+++ api/utils.py
@@ -12,11 +12,15 @@ def enviar_email(to_email, subject, body):
     if not EMAIL_USER or not EMAIL_PASS:
         return
     msg = EmailMessage()
-    msg["From"] = EMAIL_USER
-    msg["To"] = to_email
-    msg["Subject"] = subject
     msg.set_content(body)
-    
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+    msg['Subject'] = subject
+    msg['From'] = EMAIL_USER
+    msg['To'] = to_email
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.login(EMAIL_USER, EMAIL_PASS)
         server.send_message(msg)
+        server.quit()
+    except Exception as e:
+        print("Error enviando email:", e)
